6dcd7188c10a16c008b32ba3fc9af855c66b988e
Karaca00 <piywatkhonohian@gmail.com>
2025-12-26 15:59:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* */
-const CACHE_NAME = 'editor-v2.4'; // เปลี่ยนเลขเวอร์ชันตรงนี้เพื่อสั่งลบ Cache เก่าและลงไฟล์ใหม่
+/* Service Worker Master v2.4 */
+const CACHE_NAME = 'editor-v2.6.1'; 
 const ASSETS = [
   './',
   './index.html',
@@ -7,30 +7,27 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.34.1/min/vs/loader.js'
 ];
 
-// ขั้นตอนการติดตั้ง: โหลดไฟล์ใหม่ลง Cache
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting(); 
 });
 
-// ขั้นตอนการทำงาน (Activate): ลบไฟล์เว็บไซต์เดิมที่มีอยู่ใน Cache ทั้งหมด (ยกเว้นอันปัจจุบัน)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name)) // ลบ Cache เดิมทิ้ง
+          .map(name => caches.delete(name)) 
       );
     })
   );
+  self.clients.claim(); 
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 
-// ฟังคำสั่ง SKIP_WAITING เพื่อเปลี่ยนเป็นเวอร์ชันใหม่ทันที
 self.addEventListener('message', e => {
   if (e.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
